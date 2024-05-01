a78bd7ef8ed03f4e6220e790d7805f29ba3b848c
yanrbts <772166784@qq.com>
2024-05-01 18:36:03 +0800

    init code

---
+++ src/server.c
@@ -0,0 +1,213 @@
+/*
+ * Copyright (c) 2024-2024, Yanruibing <antirez at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <time.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/uio.h>
+#include <sys/un.h>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+#include <sys/resource.h>
+#include <sys/utsname.h>
+#include <locale.h>
+#include <sys/socket.h>
+
+#include "config.h"
+#include "server.h"
+#include "atomicvar.h"
+
+struct Server server;
+
+/*============================ Utility functions ============================ */
+
+/* We use a private localtime implementation which is fork-safe. The logging
+ * function of Redis may be called from other threads. */
+void nolocks_localtime(struct tm *tmp, time_t t, time_t tz, int dst);
+
+/* Low level logging. To use only for very big messages, otherwise
+ * serverLog() is to prefer. */
+void serverLogRaw(int level, const char *msg) {
+    const int syslogLevelMap[] = { LOG_DEBUG, LOG_INFO, LOG_NOTICE, LOG_WARNING };
+    const char *c = ".-*#";
+    FILE *fp;
+    char buf[64];
+    int rawmode = (level & LL_RAW);
+    int log_to_stdout = server.logfile[0] == '\0';
+
+    level &= 0xff; /* clear flags */
+    if (level < server.verbosity) return;
+
+    fp = log_to_stdout ? stdout : fopen(server.logfile, "a");
+    if (!fp) return;
+
+    if (rawmode) {
+        fprintf(fp, "%s", msg);
+    } else {
+        int off;
+        struct timeval tv;
+        // pid_t pid = getpid();
+
+        gettimeofday(&tv, NULL);
+        struct tm tm;
+        nolocks_localtime(&tm,tv.tv_sec,server.timezone,server.daylight_active);
+        off = strftime(buf,sizeof(buf),"%d %b %Y %H:%M:%S.",&tm);
+        snprintf(buf+off,sizeof(buf)-off,"%03d",(int)tv.tv_usec/1000);
+        fprintf(fp,"%d:%c %s %c %s\n", (int)getpid(), 'M', buf, c[level], msg);
+    }
+    fflush(fp);
+
+    if (!log_to_stdout) fclose(fp);
+    if (server.syslog_enabled) syslog(syslogLevelMap[level], "%s", msg);
+}
+
+/* Like serverLogRaw() but with printf-alike support. This is the function that
+ * is used across the code. The raw version is only used in order to dump
+ * the INFO output on crash. */
+void serverLog(int level, const char *fmt, ...) {
+    va_list ap;
+    char msg[LOG_MAX_LEN];
+
+    if ((level & 0xff) < server.verbosity) return;
+
+    va_start(ap, fmt);
+    vsnprintf(msg, sizeof(msg), fmt, ap);
+    va_end(ap);
+
+    serverLogRaw(level, msg);
+}
+
+/* Initialize a set of file descriptors to listen to the specified 'port'
+ * binding the addresses specified in the Redis server configuration.
+ *
+ * The listening file descriptors are stored in the integer array 'fds'
+ * and their number is set in '*count'.
+ *
+ * The addresses to bind are specified in the global server.bindaddr array
+ * and their number is server.bindaddr_count. If the server configuration
+ * contains no specific addresses to bind, this function will try to
+ * bind * (all addresses) for both the IPv4 and IPv6 protocols.
+ *
+ * On success the function returns C_OK.
+ *
+ * On error the function returns C_ERR. For the function to be on
+ * error, at least one of the server.bindaddr addresses was
+ * impossible to bind, or no bind addresses were specified in the server
+ * configuration but the function is not able to bind * for at least
+ * one of the IPv4 or IPv6 protocols. */
+int listenToPort(int port, int *fds, int *count) {
+    int j;
+
+    /* Force binding of 0.0.0.0 if no bind address is specified, always
+     * entering the loop if j == 0. */
+    if (server.bindaddr_count == 0) server.bindaddr[0] = NULL;
+    for (j = 0; j < server.bindaddr_count || j == 0; j++) {
+        if (server.bindaddr[j] == NULL) {
+            int unsupported = 0;
+            /* Bind * for both IPv6 and IPv4, we enter here only if
+             * server.bindaddr_count == 0. */
+            fds[*count] = anetTcp6Server(server.neterr, port, NULL, server.tcp_backlog);
+            if (fds[*count] != ANET_ERR) {
+                anetNonBlock(NULL,fds[*count]);
+                (*count)++;
+            } else if (errno == EAFNOSUPPORT) {
+                unsupported++;
+                serverLog(LL_WARNING,"Not listening to IPv6: unsupproted");
+            }
+
+            if (*count == 1 || unsupported) {
+                /* Bind the IPv4 address as well. */
+                fds[*count] = anetTcpServer(server.neterr, port, NULL, server.tcp_backlog);
+                if (fds[*count] != ANET_ERR) {
+                    anetNonBlock(NULL,fds[*count]);
+                    (*count)++;
+                } else if (errno == EAFNOSUPPORT) {
+                    unsupported++;
+                    serverLog(LL_WARNING,"Not listening to IPv4: unsupproted");
+                }
+                /* Exit the loop if we were able to bind * on IPv4 and IPv6,
+                * otherwise fds[*count] will be ANET_ERR and we'll print an
+                * error and return to the caller with an error. */
+                if (*count + unsupported == 2) break;
+            }
+        } else if (strchr(server.bindaddr[j],':')) {
+            /* Bind IPv6 address. */
+        } else {
+            /* Bind IPv4 address. */
+        }
+        anetNonBlock(NULL,fds[*count]);
+        (*count)++;
+    }
+    return C_OK;
+}
+
+void initServer(void) {
+    signal(SIGHUP, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);
+
+    server.port = CONFIG_DEFAULT_SERVER_PORT;
+    server.maxclients = CONFIG_DEFAULT_MAX_CLIENTS;
+    server.pid = getpid();
+    server.el = aeCreateEventLoop(server.maxclients);
+    if (server.el == NULL) {
+        serverLog(LL_WARNING,
+            "Failed creating the event loop. Error message: '%s'",
+            strerror(errno));
+        exit(1);
+    }
+    server.ipfd_count = 0;
+    server.bindaddr_count = 0;
+
+    /* Open the TCP listening socket for the user commands. */
+    if (server.port != 0 && listenToPort(server.port,server.ipfd,&server.ipfd_count) == C_ERR)
+        exit(1);
+}
+
+int main(int argc, char **argv) {
+    struct timeval tv;
+
+    setlocale(LC_COLLATE, "");
+    tzset();
+    srand(time(NULL)^getpid());
+    gettimeofday(&tv,NULL);
+
+    initServer();
+    aeMain(server.el);
+    aeDeleteEventLoop(server.el);
+    return 0;
+}
---
+++ src/server.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2024-2024, Yanruibing <antirez at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include <unistd.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <syslog.h>
+#include <netinet/in.h>
+#include <signal.h>
+
+#include "ae.h"      /* Event driven programming library */
+#include "zmalloc.h" /* total memory usage aware version of malloc/free */
+#include "anet.h"    /* Networking the easy way */
+
+/* Error codes */
+#define C_OK                            0
+#define C_ERR                           -1
+
+/* Log levels */
+#define LL_DEBUG                        0
+#define LL_VERBOSE                      1
+#define LL_NOTICE                       2
+#define LL_WARNING                      3
+#define LL_RAW                          (1<<10) /* Modifier to log without timestamp */
+
+#define CONFIG_DEFAULT_MAX_CLIENTS      10000
+#define CONFIG_BINDADDR_MAX             16
+#define CONFIG_DEFAULT_SERVER_PORT      6388  /* TCP port. */
+#define LOG_MAX_LEN                     1024 /* Default maximum length of syslog messages.*/
+
+struct Server {
+    pid_t pid;                          /* Main process pid. */
+    aeEventLoop *el;
+    unsigned int maxclients;            /* Max number of simultaneous clients */
+    /* time cache */
+    time_t unixtime;                    /* Unix time sampled every cron cycle. */
+    time_t timezone;                    /* Cached timezone. As set by tzset(). */
+    int daylight_active;                /* Currently in daylight saving time. */
+    long long mstime;                   /* Like 'unixtime' but with milliseconds resolution. */
+    /* Configuration */
+    int verbosity;                      /* Loglevel in redis.conf */
+    /* Logging */
+    char *logfile;                      /* Path of log file */
+    int syslog_enabled;                 /* Is syslog enabled? */
+    /* Networking */
+    int port;                           /* TCP listening port */
+    int tcp_backlog;                    /* TCP listen() backlog */
+    int ipfd[CONFIG_BINDADDR_MAX];      /* TCP socket file descriptors */
+    int ipfd_count;                     /* Used slots in ipfd[] */
+    char *bindaddr[CONFIG_BINDADDR_MAX]; /* Addresses we should bind to */
+    int bindaddr_count;                 /* Number of addresses in server.bindaddr[] */
+    char neterr[ANET_ERR_LEN];   /* Error buffer for anet.c */
+};
+
+int listenToPort(int port, int *fds, int *count);
+#ifdef __GNUC__
+void serverLog(int level, const char *fmt, ...)
+    __attribute__((format(printf, 2, 3)));
+#else
+void serverLog(int level, const char *fmt, ...);
+#endif
+void serverLogRaw(int level, const char *msg);
+
+#endif
